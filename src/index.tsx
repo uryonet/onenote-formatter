ac0698dbaedc2208da7618efb3adfc4aec98a1b9
yoneyama ryo <uryonet@outlook.com>
2021-06-13 13:45:09 +0900

    redux関連のファイルを作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {MsalProvider} from "@azure/msal-react"
-import reportWebVitals from './reportWebVitals';
-import {msalInstance} from "./lib/authService"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { MsalProvider } from '@azure/msal-react'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { msalInstance } from './lib/azureConfig'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,9 +13,9 @@ ReactDOM.render(
     </MsalProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/lib/azureConfig.ts
+++ src/lib/azureConfig.ts
@@ -1,13 +1,13 @@
-import {Configuration, PublicClientApplication, SilentRequest} from "@azure/msal-browser"
+import { Configuration, PublicClientApplication, SilentRequest } from '@azure/msal-browser'
 
-const authService: Configuration = {
+const azureConfig: Configuration = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID as string,
-    authority: 'https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID as string}',
-  }
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID as string}`,
+  },
 }
 
-export const msalInstance = new PublicClientApplication(authService)
+export const msalInstance = new PublicClientApplication(azureConfig)
 
 export const getToken = async (): Promise<string> => {
   const account = msalInstance.getAllAccounts()
